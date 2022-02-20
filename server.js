851ffe1dc4f7ad9c46c39699620d41aaa76d31de
Josef Rousek <josef.rousek@gmail.com>
2022-02-20 16:07:47 +0000

    Use secret

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ import * as build from "@remix-run/dev/server-build";
 const handleRequest = createPagesFunctionHandler({
   build,
   getLoadContext: (context) => {
+    console.log(context);
+    if (!context.env.COOKIE_SECRET) {
+      throw Error("Missing COOKIE_SECRET env.")
+    }
+
     const sessionStorage = createCloudflareKVSessionStorage({
       cookie: {
         name: "SESSION_ID",
-        secrets: ["YOUR_COOKIE_SECRET"],
+        secrets: [context.env.COOKIE_SECRET],
         secure: true,
         sameSite: "strict",
+        maxAge: 14 * 24 * 60 * 60
       },
       kv: context.env.sessionStorage,
     })
